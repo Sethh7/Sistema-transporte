78f48d9e59360b23bc1ff1b9b517f280d9032167
Adriel <adrielrodrigues156@gmail.com>
2026-06-10 23:44:29 -0300

    V1 - Conexão com o Python

---
+++ conexão_python/conxecao.py
@@ -0,0 +1,27 @@
+import psycopg2
+
+print("Tentando conectar ao banco de dados...")
+
+try:
+    # Tenta conectar direto, sem bibliotecas intermediárias
+    conexao = psycopg2.connect(
+        host="localhost",
+        port="5432",
+        database="postgres", # Coloque aqui o nome correto do banco
+        user="postgres",
+        password="gustavo123" # Verifique se essa é mesmo a senha
+    )
+    print("✅ SUCESSO! A conexão está perfeita!")
+    
+
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM  alocacao;") # MUDE ESTE NOME
+    print("SUCESSO! A tabela existe e foi encontrada!")
+    
+    conexao.close()
+
+except Exception as erro_real:
+    print("\nA CONEXÃO OU A CONSULTA FALHOU!")
+    print("Aqui está o erro verdadeiro (com os acentos zoados, mas legível):")
+    # O comando repr() impede que o Python tente decodificar o 'ç' e trave
+    print(repr(erro_real))
