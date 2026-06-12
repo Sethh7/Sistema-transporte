24a5cab71fa3599b97012c10b51b55f2efcf3954
Gustavo-faraujo <gusfaraujo@gmail.com>
2026-06-12 18:14:01 -0300

    Update and rename linha_parada to linha_parada.sql

---
+++ Scripts/População/linha_parada.sql
@@ -0,0 +1,60 @@
+INSERT INTO LINHA_PARADA (tempo_estimado, fk_id_linha, fk_id_ponto) VALUES 
+-- LINHA 1: GAR-101 (Origem: 1 -> Destino: 11)
+(140, 1, 11),
+
+-- LINHA 2: GAR-102 (Origem: 11 -> Destino: 1)
+(140, 2, 1),
+
+-- LINHA 3: GAR-201 (Origem: 2 -> Destino: 11)
+(30, 3, 3),
+
+-- LINHA 4: CAR-301 (Origem: 11-> Destino: 2)
+(30, 4, 10),
+
+-- LINHA 5: CAR-302 (Origem: 3 -> Destino: 11)
+(110, 5, 16),
+
+-- LINHA 6: CAR-401 (Origem: 11 -> Destino: 3)
+(110, 6, 11),
+
+-- LINHA 7: POLO-01 (Origem: 4 -> Destino: 11)
+(30, 7, 11),
+
+-- LINHA 8: POLO-02 (Origem: 11 -> Destino: 4)
+(30, 8, 21),
+
+-- LINHA 9: JEANS-01 (Origem: 5 -> Destino: 11)
+(20, 9, 22),
+
+-- LINHA 10: JEANS-02 (Origem: 11 -> Destino: 5)
+(20, 10, 21),
+
+-- LINHA 11: AGR-001 (Origem: 6 -> Destino: 11)
+(90, 11, 3),
+
+-- LINHA 12: AGR-002 (Origem: 11 -> Destino: 6)
+(90, 12, 11),
+
+-- LINHA 13: BJ-CAR (Origem: 7 -> Destino: 11)
+(40, 13, 27),
+
+-- LINHA 14: GRA-BEZ (Origem: 11 -> Destino: 7)
+(40, 14, 29),
+
+-- LINHA 15: PES-LAJ (Origem: 8 -> Destino: 11)
+(90, 15, 32),
+
+-- LINHA 16: FNAV-01 (Origem: 11 -> Destino: 8)
+(90, 20, 40),
+
+-- LINHA 17: LAJ-GAR (Origem: 9 -> Destino: 11)
+(50, 16, 34),
+
+-- LINHA 18: BC-GAR (Origem: 11 -> Destino: 9)
+(50, 17, 37),
+
+-- LINHA 19: SBU-BJ (Origem: 10 -> Destino: 11)
+(130, 18, 38),
+
+-- LINHA 20: BMD-CAR (Origem: 11 -> Destino: 10)
+(130, 19, 39);
