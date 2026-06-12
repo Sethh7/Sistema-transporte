e41a4fb223b39246b74e3c894985dc76541e395d
Gustavo-faraujo <gusfaraujo@gmail.com>
2026-06-12 16:11:51 -0300

    Update viagem.sql

--- Scripts/Criação/viagem.sql
+++ Scripts/Criação/viagem.sql
@@ -1,7 +1,10 @@
 CREATE TABLE viagem (
 	id_viagem INTEGER PRIMARY KEY,
+	data_viagem TIMESTAMP,
 	fk_id_onibus INTEGER,
 	fk_id_motorista INTEGER,
+	fk_id_horario INTEGER,
 	FOREIGN KEY (fk_id_onibus) REFERENCES onibus (id_onibus),
-	FOREIGN KEY (fk_id_motorista) REFERENCES motorista (id_motorista)
+	FOREIGN KEY (fk_id_motorista) REFERENCES motorista (id_funcionario),
+	FOREIGN KEY (fk_id_horario) REFERENCES horario_linha (id_horario)
 );
