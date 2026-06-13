78c985d82ea476b426b86a3ea02ac3d7927772a5
Gustavo-faraujo <gusfaraujo@gmail.com>
2026-06-13 00:24:46 -0300

    Update ponto_parada.sql

--- Scripts/Criação/ponto_parada.sql
+++ Scripts/Criação/ponto_parada.sql
@@ -1,7 +1,6 @@
 CREATE TABLE ponto_parada (
 	id_ponto INTEGER PRIMARY KEY,
 	nome VARCHAR(100),
-	coberto BOOLEAN,
 	fk_id_bairro INTEGER,
 	FOREIGN KEY (fk_id_bairro) REFERENCES bairro (id_bairro)
 );
